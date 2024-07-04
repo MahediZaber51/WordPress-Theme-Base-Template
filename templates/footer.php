eb0fec2c0c7e2f744dbd8fd0f30051767f6c52c7
Mahedi Zaman Zaber <88625958+MahediZaber51@users.noreply.github.com>
2024-07-04 13:47:42 +0600

    Footer template

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The footer template
+ *
+ * @package Your_Theme
+ * @link https://github.com/MahediZaber51/WordPress-Theme-Base-Template
+ */
+?>
+
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
